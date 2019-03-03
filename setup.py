de6687a38130ef45971ec6406b0007bb9beaf321
salpreh <salva.perez46@gmail.com>
2019-03-03 17:21:00 +0100

    Setup version updated
    -Version release due to an error in doc display in Pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='tablat',
-    version='0.1.1',
+    version='0.1.2',
     description='Print basic tables in python',
     long_description=readme,
     long_description_content_type='text/markdown',
