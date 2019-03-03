84661727f8f0dae291d0b67a46f93c1ed345f8d4
salpreh <salva.perez46@gmail.com>
2019-03-03 16:53:08 +0100

    Setup version updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='tablat',
-    version='0.1.0',
+    version='0.1.1',
     description='Print basic tables in python',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -14,5 +14,5 @@ setup(
     author_email='salva.perez46@gmail.com',
     url='https://github.com/salpreh/tablat',
     license='MIT License',
-    packages=find_packages(exclude=('test', 'assets', 'venv'))
+    packages=find_packages(exclude=('test', 'assets', 'venv', 'doc'))
 )
