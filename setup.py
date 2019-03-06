ce193dd087a8c96e452a1fba3c4ac4402110d00a
salpreh <salva.perez46@gmail.com>
2019-03-06 20:36:14 +0100

    Setup version updated
    - Modifications in setup script to automate build generation

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 # -*- encoding: utf-8 -*-
 from setuptools import setup, find_packages
+from pathlib import Path
+import re
 
+
+# Get current version
+version = '0.1.3'
+version_input_path = Path('../setup/version.txt')
+if version_input_path.exists():
+    with open(version_input_path, 'r') as f:
+        version_input = f.readline().rstrip()
+
+    if re.match('\\d+\\.\\d+\\.\\d+', version_input):
+        version = version_input
+
+# Load doc
 with open('README.md') as f:
     readme = f.read()
 
 setup(
     name='tablat',
-    version='0.1.2',
+    version=version,
     description='Print basic tables in python',
     long_description=readme,
     long_description_content_type='text/markdown',
